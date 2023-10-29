5a13cfecff1a25a1f8648fa67ef9aacd426c2442
Noodlealvel <alvelasco2001@gmail.com>
2023-10-29 20:46:42 +0100

    Creación de templates html

--- cuentas/views.py
+++ cuentas/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render (request, "cuentas/index.html")
+
+def signup(request):
+    return render (request, "cuentas/signup.html")
+
+def login(request):
+    return render (request, "cuentas/login.html")
+
+def logout(request):
+    pass
