146c624ef7217baa1a213e1ea5200b10bdecdad4
Noodlealvel <alvelasco2001@gmail.com>
2023-11-08 22:55:27 +0100

    Terminado modificar datos y logout

--- cuentas/urls.py
+++ cuentas/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('signup', views.signup, name="signup"),
     path('signin', views.signin, name="signin"),
-    path('logout', views.logout, name="logout"),
-    path('verifyPassword', views.verifyPassword, name="verifyPassword"),
-   # path('changeParameters', views.changeParameters, name="changeParameters"),
-    #path('modify', views.modify, name="modify"),
+    path('logout', views.log_out, name="logout"),
+    path('modifyParameters', views.modifyParameters, name="modifyParameters"),
 ]
--- cuentas/views.py
+++ cuentas/views.py
@@ -1,8 +1,10 @@
+from re import A
 from turtle import end_fill
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 # Create your views here.
@@ -18,7 +20,22 @@ def signup(request):
         email=request.POST.get('email')
         passwd=request.POST.get('passwd')
         passwd2=request.POST.get('passwd2')
+        if (not username or not firstname or not lastname or not email or not passwd or not passwd2):
+            messages.error(request, "All fields are required.")
+            return redirect ("signup")
 
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists.")
+            return redirect ("signup")
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "E-mail already has another account associated.")
+            return redirect ("signup")
+        
+        if passwd!=passwd2:
+            messages.error(request, "Passwords don't match.")
+            return redirect ("signup")
+        
         user = User.objects.create_user(username, email, passwd)
         user.first_name=firstname
         user.last_name=lastname
@@ -49,17 +66,62 @@ def signin(request):
     return render (request, "cuentas/login.html")
 
 @login_required
-def verifyPassword(request):
+def modifyParameters(request):
     if request.method == 'POST':
-        password = request.get('password')
-        if request.user.check_password(password):
-            return render(request, "cuentas/modifyParameters.html")
-        else:
-            messages.error(request, "Incorrect password.")
-            return render(request, 'cuentas/verifyPassword.html')
+        if request.POST.get('verify')=="verify":
+            password = request.POST.get('password')
+            if request.user.check_password(password):
+                return render(request, "cuentas/modifyParameters.html", {'verified': 'true'})
+            else:
+                messages.error(request, "Incorrect password.")
+                return redirect("modifyParameters")
+        elif request.POST.get('delete')=="delete": #borrar usuario
+            user = request.user
+            logout(request)
+            user.delete()
+            messages.success(request, "Your account has been deleted.")  
+            return redirect ('home')
+        else: #actualizar datos
+            user=request.user
+            username= request.POST.get('username')
+            firstname=request.POST.get('firstname')
+            lastname=request.POST.get('lastname')
+            email=request.POST.get('email')
+            passwd=request.POST.get('passwd')
+            passwd2=request.POST.get('passwd2')
+            if (not username or not firstname or not lastname or not email or not passwd or not passwd2):
+                messages.error(request, "All fields are required.")
+                return render(request, "cuentas/modifyParameters.html", {'verified': 'true'})
+
+            if user.get_username()!=username:
+                if User.objects.filter(username=username).exists():
+                    messages.error(request, "Username already exists.")
+                    return render(request, "cuentas/modifyParameters.html", {'verified': 'true'})
+
+            if user.email!=email:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, "E-mail already has another account associated.")
+                    return render(request, "cuentas/modifyParameters.html", {'verified': 'true'})
+            
+            if passwd!=passwd2:
+                messages.error(request, "Passwords don't match.")
+                return render(request, "cuentas/modifyParameters.html", {'verified': 'true'})
+
+            user.username=username
+            user.set_password(passwd)
+            user.email=email
+            user.first_name=firstname
+            user.last_name=lastname
+
+            user.save()
+
+            messages.success(request, "Your account's parameters were modified succesfully.")
+
+            return redirect ('home')
     else:
-        return render(request, "cuentas/verifyPassword.html")
+        return render(request, "cuentas/modifyParameters.html")
 
-
-def logout(request):
-    pass
+@login_required
+def log_out(request):
+    logout(request)
+    return render(request, "cuentas/index.html")
